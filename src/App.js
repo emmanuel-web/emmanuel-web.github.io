f82fddf493443409561e74fc8f7ce0208f02f41f
Emmanuel Garzon <emmanuelgarzon2001@hotmail.com>
2024-03-05 21:03:34 -0500

    the header has been added in app corrected

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
-import header from './components/Header';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header/>
+      <Header/>
     </div>
   );
 }
