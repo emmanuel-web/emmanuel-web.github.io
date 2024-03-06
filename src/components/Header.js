ff2377b412308c0201f9bd64530396d3817dfa8a
Emmanuel Garzon <emmanuelgarzon2001@hotmail.com>
2024-03-05 20:54:00 -0500

    the header has been added

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <h1>Emmanuel Garzón Cabarcas</h1>
+      <p>Software Developer</p>
+      
+    </header>
+  );
+}
+
+export default Header;
